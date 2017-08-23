3d66b0e6e3af6cff8e4a8f2ba9651c11c92a3245
Harlan Howell <lin_032@hotmail.com>
2017-08-23 14:37:05 +0800

    xlsb2xlsx.js

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # simple_tools
 
-# excel 转成 plist 
+## export_plist.py: excel 转成 plist 
 ```sh
 ./export_plist.py test.xlsx # 默认输出同名文件.plist
 ./export_plist.py test.xlsx output.plist # 指定输出文件
@@ -10,3 +10,4 @@ pip install openpyxl
 
 *pip*: https://pip.readthedocs.io/en/stable/installing/
 
+## xlsb2xlsx.js: xlsb 转 xlsx
---
+++ xlsb2xlsx.js
@@ -0,0 +1,20 @@
+var xlsx = require("xlsx")
+var fs = require("fs")
+var path = require("path")
+
+var curPath = path.resolve(__dirname) 
+var files = fs.readdirSync(curPath)
+
+console.log("js " + curPath)
+var file_extension = ".xlsb"
+for (var index in files) {
+    var filename = files[index]
+    if (filename.endsWith(file_extension)) {
+        var inpath = path.join(curPath, filename)
+        var outpath = path.join(curPath, filename.substring(0, filename.length - file_extension.length) + ".xlsx")
+
+        console.log("convert: " + inpath)
+        var wb = xlsx.readFile(inpath)
+        xlsx.writeFile(wb, outpath)
+    }
+}
